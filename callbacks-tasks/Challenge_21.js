c22fa6b9f6b03d9a94fa0057bae7ef522209c7bd
Cube-Cb <79654242+Cube-Cb@users.noreply.github.com>
2021-03-02 21:26:10 +0500

    added 22 task

--- callbacks-tasks/Challenge_21.js
+++ callbacks-tasks/Challenge_21.js
@@ -18,3 +18,14 @@ function highestFunc(objOfFuncs, subject) {
 // console.log(highestFunc(groupOfFuncs, 11)); // should log: 'double'
 // console.log(highestFunc(groupOfFuncs, -20)); // should log: 'inverse'
 */
+
+function highestFunc(objOfFuncs, subject) {
+}
+
+const groupOfFuncs = {};
+groupOfFuncs.double = n => n * 2;
+groupOfFuncs.addTen = n => n + 10;
+groupOfFuncs.inverse = n => n * -1;
+console.log(highestFunc(groupOfFuncs, 5)); // should log: 'addTen'
+console.log(highestFunc(groupOfFuncs, 11)); // should log: 'double'
+console.log(highestFunc(groupOfFuncs, -20)); // should log: 'inverse'

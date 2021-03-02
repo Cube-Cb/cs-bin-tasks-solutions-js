86410b68629af4bebd38592c02818a8dd082c3c2
Cube-Cb <79654242+Cube-Cb@users.noreply.github.com>
2021-03-02 19:33:56 +0500

    added 20 tasks

---
+++ callbacks-tasks/Challenge_20.js
@@ -0,0 +1,20 @@
+/*
+Challenge 20
+Create a function pipe that accepts an array (of functions) and a value. 
+pipe should input the value into the first function in the array, 
+and then use the output from that function as input for the second function, 
+and then use the output from that function as input for the third function, 
+and so forth, until we have an output from the last function in the array. 
+pipe should return the final output.
+
+function pipe(arrOfFuncs, value) {
+
+}
+
+//  Uncomment these to check your work!
+// const capitalize = str => str.toUpperCase();
+// const addLowerCase = str => str + str.toLowerCase();
+// const repeat = str => str + str;
+// const capAddlowRepeat = [capitalize, addLowerCase, repeat];
+// console.log(pipe(capAddlowRepeat, 'cat')); // should log: 'CATcatCATcat'
+*/
